452fd537a61fb6db504a923270fdf511be0a15d9
Sam <kumar_samyak@outlook.com>
2023-10-27 22:23:31 -0500

    Implemented OpenAI

--- helper.py
+++ helper.py
@@ -1,6 +1,16 @@
+import collections 
+try:
+    from collections.abc import MutableSet, MutableMapping
+    collections.MutableSet = MutableSet
+    collections.MutableMapping = MutableMapping
+except Exception:
+    from collections import MutableSet, MutableMapping
+
 from argparse import ArgumentParser, Namespace
 from PyPDF2 import PdfReader
-from os import system
+from os import system, getenv
+import openai
+from dotenv import load_dotenv
 
 
 def get_args() -> Namespace:
@@ -9,12 +19,27 @@ def get_args() -> Namespace:
     Returns:
         Namespace: The "list" of arguments
     """
-    parser = ArgumentParser(prog="main", description="Read a PDF contents to a TXT")
+    parser = ArgumentParser(prog="summarizer", description="Read a PDF contents to a TXT")
 
     parser.add_argument('input_pdf', type=str, nargs=1, help="Input PDF file")
     parser.add_argument('output_txt', type=str, nargs='*', default="output.txt", help="Output TXT file")
 
     return parser.parse_args()
+
+
+def OCR_pdf(file: str) -> str:
+    """Converts standard PDF to OCR PDF/A which can be read by PyPDF2
+
+    Args:
+        file (str): The filename of the PDF/A
+
+    Returns:
+        str: The filename of the OCR PDF/A
+    """
+    print(f"OCR'ing {file}")
+    system(f"ocrmypdf '{file}' '{file}' --deskew --skip-text --quiet")
+    print(f"OCR'd {file}")
+    return file
 
 
 def get_pdf_text(file: str) -> str:
@@ -27,22 +52,46 @@ def get_pdf_text(file: str) -> str:
         str: The text from the PDF/A
     """
     reader = PdfReader(OCR_pdf(file))
-    
+
+    print(f"Reading {file}")
+
     text = ""
     for page in reader.pages:
         text += page.extract_text()
-    
+
+    print(f"Read {file}")
     return text
 
 
-def OCR_pdf(file: str) -> str:
-    """Converts standard PDF to OCR PDF/A which can be read by PyPDF2
+def summarize_text(text: str) -> str:
+    load_dotenv()
+    openai.api_key = getenv("OPENAI_API_KEY")
+    
+    response = openai.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=f"Summarize the content you are given for a high school student. The text is {text[0:1000]}",
+        temperature=0,
+        max_tokens=4096 - 231,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    
+    return response.choices[0].text
+
+
+def write_text(text: str, file: str) -> None:
+    """Writes the text to a file
 
     Args:
-        file (str): The filename of the PDF/A
-
-    Returns:
-        str: The filename of the OCR PDF/A
+        text (str): The text to write
+        file (str): The filename to write to
     """
-    system(f"ocrmypdf '{file}' '{file}' --deskew")
-    return file
+    print(f"Writing {file}")
+
+    with open(file, "w") as f:
+        f.write(text)
+
+    print(f"Wrote {file}")
+
+
--- summarizer.py
+++ summarizer.py
@@ -5,7 +5,8 @@ from argparse import Namespace
 def main() -> None:
     args: Namespace = get_args()
     text: str = get_pdf_text(args.input_pdf[0])
-    print(text)
+    text: str = summarize_text(text)
+    write_text(text, args.output_txt)
     
 
 if __name__ == "__main__":
